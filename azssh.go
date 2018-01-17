dbee2f5feb7c2d1326b26274327cd2db9bbe40f3
Mike Staszel <mikestaszel@gmail.com>
2018-01-16 22:05:17 -0600

    describing instances

--- azssh.go
+++ azssh.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
 func main() {
-	print("hello world")
+	sess := session.Must(session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable}))
+	ec2_service := ec2.New(sess)
+
+	result, err := ec2_service.DescribeInstances(nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Success!")
+		fmt.Println(result)
+	}
 }
