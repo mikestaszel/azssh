13029a24f0c3c23212bf50d8b1f9b579e81ccbc4
Mike Staszel <mike@mikestaszel.com>
2018-01-28 19:01:16 -0600

    actually ssh into instances, add more command verbs

--- azssh.go
+++ azssh.go
@@ -6,9 +6,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/aws"
+	"os/exec"
 )
 
-// Get the instance by name.
+// Get an instance by name.
 func getInstanceByName(ec2Service *ec2.EC2, instanceName string) (*ec2.Instance, error) {
 	result, err := ec2Service.DescribeInstances(nil)
 
@@ -94,23 +95,36 @@ func main() {
 		instance, _ := getInstanceByName(ec2Service, instanceName)
 
 		// TODO: if the instance is off, prompt user - should it be turned on? if yes, wait 60 seconds then retry
+		// TODO: do something if publicDns is an empty string!
 
 		publicDns, _ := getPublicDns(instance)
 		sshCmd := "ssh ubuntu@" + publicDns
-		fmt.Println(sshCmd)
-	} else if len(os.Args) == 3 && os.Args[1] == "up" {
+		fmt.Println("running command: ", sshCmd)
+
+		cmd := exec.Command("ssh", "ubuntu@" + publicDns)
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	} else if len(os.Args) == 3 && os.Args[1] == "dns" {
+		instanceName := os.Args[2]
+		instance, _ := getInstanceByName(ec2Service, instanceName)
+		publicDns, _ := getPublicDns(instance)
+		fmt.Println(publicDns)
+	} else if len(os.Args) == 3 && (os.Args[1] == "up" || os.Args[1] == "start") {
 		instanceName := os.Args[2]
 		instance, _ := getInstanceByName(ec2Service, instanceName)
 		startInstance(ec2Service, instance)
-	} else if len(os.Args) == 3 && os.Args[1] == "down" {
+	} else if len(os.Args) == 3 && (os.Args[1] == "down" || os.Args[1] == "shutdown" || os.Args[1] == "stop") {
 		instanceName := os.Args[2]
 		instance, _ := getInstanceByName(ec2Service, instanceName)
 		stopInstance(ec2Service, instance)
-	} else if len(os.Args) == 3 && os.Args[1] == "reboot" {
+	} else if len(os.Args) == 3 && (os.Args[1] == "reboot" || os.Args[1] == "restart") {
 		instanceName := os.Args[2]
 		instance, _ := getInstanceByName(ec2Service, instanceName)
 		rebootInstance(ec2Service, instance)
 	} else {
+		// TODO: Write some help text!
 		fmt.Println("Invalid parameters!")
 		os.Exit(1)
 	}
