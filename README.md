59b5cfbc66d4e7e7556248dca3a8cddf0960d37f
Mike Staszel <mike@mikestaszel.com>
2018-02-13 20:04:36 -0600

    ask for username for ssh, raise more errors, add usage to readme

--- README.md
+++ README.md
@@ -4,5 +4,52 @@
 # azssh #
 Tool to SSH into EC2 instances by name and perform basic operations such as starting, stopping, and rebooting.
 
+This tool gets instance public DNS addresses and "Name" tag values and allows you to list, start, stop, restart, or SSH
+into instances.
+
+# Usage #
+
+To use this tool, you must have AWS credentials configured.
+[This AWS guide](https://docs.aws.amazon.com/cli/latest/userguide/cli-environment.html) will help you get set up.
+
+Simply download the binary from the [GitHub release page](https://github.com/mikestaszel/azssh/releases) or clone
+and `go build` the source to get started.
+
+### Listing Instances ###
+
+To list instances, run the following:
+
+    azssh ls
+
+### Starting an Instance ###
+
+To start an instance, run the following:
+
+    azssh start instance_name
+
+### Stopping an Instance ###
+
+To stop an instance, run the following:
+
+    azssh stop instance_name
+
+### Restarting an Instance ###
+
+To restart an instance, run the following:
+
+    azssh restart instance_name
+
+### Getting Public DNS of an Instance ###
+
+To print the public DNS address of an instance, run the following:
+
+    azssh dns instance_name
+
+### SSHing into an Instance ###
+
+To SSH into an instance, run the following:
+
+    azssh ssh instance_name
+
 # License #
 See the LICENSE file.
--- azssh.go
+++ azssh.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"os"
 	"os/exec"
+	"bufio"
+	"strings"
 )
 
 // Get a list of instances.
@@ -132,13 +134,13 @@ func rebootInstance(ec2Service *ec2.EC2, instance *ec2.Instance) error {
 
 func printUsage() {
 	fmt.Println("azssh: utility to manage EC2 instances")
-	fmt.Println("usage: azssh ssh instanceName        SSH into instance")
-	fmt.Println("   or: azssh ls                      list instances")
-	fmt.Println("   or: azssh dns                     print public DNS of instance")
-	fmt.Println("   or: azssh up instanceName         start this instance public DNS of instance")
-	fmt.Println("   or: azssh down instanceName       print public DNS of instance")
-	fmt.Println("   or: azssh reboot instanceName     print public DNS of instance")
-	fmt.Println("   or: azssh help                    print this help text")
+	fmt.Println("usage: azssh ssh instance_name         SSH into instance")
+	fmt.Println("   or: azssh ls                        list instances")
+	fmt.Println("   or: azssh dns                       print public DNS of instance")
+	fmt.Println("   or: azssh start instance_name       start this instance public DNS of instance")
+	fmt.Println("   or: azssh stop instance_name        print public DNS of instance")
+	fmt.Println("   or: azssh restart instance_name     print public DNS of instance")
+	fmt.Println("   or: azssh help                      print this help text")
 }
 
 // Main function.
@@ -150,20 +152,34 @@ func main() {
 		instanceName := os.Args[2]
 		instance, _ := getInstanceByName(ec2Service, instanceName)
 
-		// TODO: if the instance is off, prompt user - should it be turned on? if yes, wait 60 seconds then retry
-		// TODO: do something if publicDns is an empty string!
-
 		publicDns, _ := getInstancePublicDns(*instance)
-		sshCmd := "ssh ubuntu@" + publicDns
+
+		if publicDns == "" {
+			fmt.Println("Instance does not have a public DNS address or is stopped.")
+			os.Exit(1)
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Username? [ubuntu]: ")
+		username, _ := reader.ReadString('\n')
+		username = strings.TrimSuffix(username, "\n")
+		if username == "" {
+			username = "ubuntu"
+		}
+
+		sshCmd := fmt.Sprintf("ssh %s@%s", username, publicDns)
 		fmt.Println("running command: ", sshCmd)
 
-		cmd := exec.Command("ssh", "ubuntu@"+publicDns)
+		cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", username, publicDns))
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
 		cmd.Stderr = os.Stderr
 		cmd.Run()
 	} else if len(os.Args) == 2 && os.Args[1] == "ls" {
-		instances, _ := getInstances(ec2Service)
+		instances, err := getInstances(ec2Service)
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println("instances:")
 		for _, instance := range instances {
 			instanceName, _ := getInstanceName(instance)
@@ -172,20 +188,35 @@ func main() {
 		}
 	} else if len(os.Args) == 3 && os.Args[1] == "dns" {
 		instanceName := os.Args[2]
-		instance, _ := getInstanceByName(ec2Service, instanceName)
-		publicDns, _ := getInstancePublicDns(*instance)
+		instance, err := getInstanceByName(ec2Service, instanceName)
+		if err != nil {
+			panic(err)
+		}
+		publicDns, err := getInstancePublicDns(*instance)
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println(publicDns)
 	} else if len(os.Args) == 3 && (os.Args[1] == "up" || os.Args[1] == "start") {
 		instanceName := os.Args[2]
-		instance, _ := getInstanceByName(ec2Service, instanceName)
+		instance, err := getInstanceByName(ec2Service, instanceName)
+		if err != nil {
+			panic(err)
+		}
 		startInstance(ec2Service, instance)
 	} else if len(os.Args) == 3 && (os.Args[1] == "down" || os.Args[1] == "shutdown" || os.Args[1] == "stop") {
 		instanceName := os.Args[2]
-		instance, _ := getInstanceByName(ec2Service, instanceName)
+		instance, err := getInstanceByName(ec2Service, instanceName)
+		if err != nil {
+			panic(err)
+		}
 		stopInstance(ec2Service, instance)
 	} else if len(os.Args) == 3 && (os.Args[1] == "reboot" || os.Args[1] == "restart") {
 		instanceName := os.Args[2]
-		instance, _ := getInstanceByName(ec2Service, instanceName)
+		instance, err := getInstanceByName(ec2Service, instanceName)
+		if err != nil {
+			panic(err)
+		}
 		rebootInstance(ec2Service, instance)
 	} else if len(os.Args) == 2 && (os.Args[1] == "help") {
 		printUsage()
